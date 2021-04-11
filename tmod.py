cbc6da4f1e65c4863e557b09f082c1454b2217f0
outlaws42 <outlaws42@gmail.com>
2021-04-11 07:25:28 -0400

    added funtion to convert string time to timestamp

--- tmod.py
+++ tmod.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 # -*- coding: utf-8 -*-
-version = '2021-04-09'
+version = '2021-04-11'
 
 # Imports included with python
 import random
@@ -232,19 +232,28 @@ def reverse_sublist(self,list_):
         list_[i][:] = list_[i][::-1]
     return list_
     
-def last_n_lines(fname, lines):
+def last_n_lines(fname, lines, fdest='relative'):
   """
   Gets the last so many lines of a file 
   and returns those lines in text.
   Arguments = filename, number of lines
   """
   home = os.path.expanduser("~")
-  file_lines = []
-  with open(f'{home}/{fname}') as file:
-    for line in (file.readlines() [-lines:]):
-      file_lines.append(line)
+  try:
+    file_lines = []
+    if fdest == 'home' or fdest == 'Home':
+      with open(f'{home}/{fname}') as file:
+        for line in (file.readlines() [-lines:]):
+          file_lines.append(line)
+    else:
+      with open(get_resource_path(fname), 'r') as file:
+        for line in (file.readlines() [-lines:]):
+          file_lines.append(line)
     file_lines_text = (''.join(file_lines))
     return file_lines_text
+  except(FileNotFoundError) as e:
+    print(e)
+    return 'file not found'
 
 # Date/Time//////////////////////////////////////////////
 def day_diff(month,day,year):
@@ -262,7 +271,22 @@ def time_now():
     current =  datetime.now().strftime('%H:%M:%S')
     return current
 
-   
+def timestamp_from_string_time(str_time):
+    '''
+    Takes a string time with AM or PM and 
+    converts it to a Unix timestamp
+    '''
+    dt_time = datetime.strptime(str_time, '%I:%M %p').time()
+    today_date = datetime.today().date()
+    str_date = today_date.strftime('%Y-%m-%d')
+    year, month, day = str_date.split('-')
+    dt = datetime.combine(
+      date(int(year), int(month), int(day)), dt_time)
+    ts = int(dt.timestamp())
+    return ts
+
+
+
 def import_temp(file_='temp.txt'):
     '''
     Import temp from text file then split off each word. 
@@ -301,7 +325,7 @@ def try_float(temp):
     return temp
     
 # file information
-def check_file_age(fname):
+def check_file_age(fname, fdest='relative'):
   """
   Returns the difference of the current timestamp and the
   timestamp of a file last write in hours 
@@ -309,7 +333,10 @@ def check_file_age(fname):
   Requires import os
   """
   home = os.path.expanduser("~")
-  file_info= os.stat(f'{home}/{fname}')
+  if fdest == 'home' or fdest == 'Home':
+    file_info= os.stat(f'{home}/{fname}')
+  else:
+    file_info= os.stat(get_resource_path(fname))
   now = datetime.now().timestamp()
   modified = int(file_info.st_mtime)
   difference_hour = int(((now - modified)/60)/60)
